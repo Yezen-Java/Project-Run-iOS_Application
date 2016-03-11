5be427d944c2d575ce742f5e928b8f6d3a30c420
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-11 12:29:58 +0000

    added tour code validation

--- IosApp/database/TourIdValidation.php
+++ IosApp/database/TourIdValidation.php
@@ -5,7 +5,6 @@ include 'Connect.php';
 
 $tourId = $_POST['TourId'];
 
-
 $query = "SELECT * From tour where tourid = $1";
 
 $result = pg_prepare($dbconn,"Tour_query", $query);
@@ -18,6 +17,7 @@ $result = pg_execute($dbconn, "Tour_query", array($tourId));
 
 		$rows = pg_fetch_array($result);
 		$tourIdPg = $rows['tourId'];
+		echo json_encode(array('exists' => pg_num_rows($result)> 0));
 		echo "success";	
 
 
