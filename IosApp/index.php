5b18a193494513b242efd21419ec8a57a492a737
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-11 14:25:47 +0000

    add

--- IosApp/index.php
+++ IosApp/index.php
@@ -89,7 +89,7 @@
 			    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1>Processing...</h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
 			    return {
 			        showPleaseWait: function() {
-			            pleaseWaitDiv.modal();
+			            pleaseWaitDiv.modal('show');
 			        },
 			        hidePleaseWait: function () {
 			            pleaseWaitDiv.modal('hide');
