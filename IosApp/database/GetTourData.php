7d19aefcd83f9084e410fb7c7207805b2b4464ac
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-11 14:08:55 +0000

    add

--- IosApp/database/GetTourData.php
+++ IosApp/database/GetTourData.php
@@ -8,7 +8,7 @@ $tourId = $_POST['TourID'];
 $escape = pg_escape_string($tourId);
 
 
-$query = "SELECT * From tour where tourid = $1";
+$query = "SELECT * from tour_res, location where tour_res.tourid = $1 and tour_res.locationid = location.locationid;";
 
 $result = pg_prepare($dbconn,"TourData_query", $query);
 
@@ -22,10 +22,12 @@ if (pg_num_rows($result)>0) {
 
 	while($rows=pg_fetch_array($result)){
 
+		$name = $row['lname'];
+		
 		echo "<div class='span2'>
 			 
-          <button onclick='show('Page3');' style='text-decoration: none'  class='pageButtons btn-default btn-lg btn-block' >
-      		<span ><img class='imageButtons' img-block src='images/mriMachine.jpg' width='600px' align='left' ><p class='imgTextTop' align='left'>Cardiac</p><p align='left' class='imgBottomText' >North Wing</p>
+          <button onclick='show('Page3');' style='text-decoration: none' class='pageButtons btn-default btn-lg btn-block' >
+      		<span ><img class='imageButtons' img-block src='images/mriMachine.jpg' width='600px' align='left' ><p class='imgTextTop' align='left'>$name</p><p align='left' class='imgBottomText' >North Wing</p>
 			    </span> 
 		      </button> 
 		    </div>";
