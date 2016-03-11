6e69ca8d494b95b4433229671571d4652428addd
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-11 14:41:12 +0000

    add

--- IosApp/database/GetTourData.php
+++ IosApp/database/GetTourData.php
@@ -23,10 +23,11 @@ if (pg_num_rows($result)>0) {
 	while($rows=pg_fetch_array($result)){
 
 		$name = $rows['lname'];
+		$id = $rows['locationid'];
 
 		echo "<div class='span2'>
 			 
-          <button onclick='show('Page3');' style='text-decoration: none' class='pageButtons btn-default btn-lg btn-block'>
+          <button onclick='show('Page3');' value=''style='text-decoration: none' class='pageButtons btn-default btn-lg btn-block'>
       		<span ><img class='imageButtons' img-block src='images/mriMachine.jpg' width='600px' align='left' ><p class='imgTextTop' align='left'>$name</p><p align='left' class='imgBottomText' >North Wing</p>
 			    </span> 
 		      </button> 
