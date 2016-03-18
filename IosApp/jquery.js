5e92fd54e76182419c69f815b083fc57d60a3542
abdi <[nasir2891@googlemail.com]>
2016-03-18 01:51:32 +0000

    Testing some code

--- IosApp/jquery.js
+++ IosApp/jquery.js
@@ -40,8 +40,8 @@ document.addEventListener('touchstart',function(e) {
 document.addEventListener('touchmove',function(e) {
     var xMovement = Math.abs(e.touches[0].screenX - xStart);
     var yMovement = Math.abs(e.touches[0].screenY - yStart);
-    if((yMovement * 5) > xMovement) {
-        e.preventDefault();
+    if((yMovement * 3) > xMovement) {
+//        e.preventDefault();
     }
 });
 //................................Test code end 
