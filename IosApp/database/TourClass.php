988d4e1660ab4a559e64c0d7462c5af02502146a
Yezen-Java <yezen.alnafei@kcl.ac.uk>
2016-03-18 13:30:56 +0000

    added tour class

--- IosApp/database/TourClass.php
+++ IosApp/database/TourClass.php
@@ -24,7 +24,7 @@ class TourClass
 	public function getTourLocation($tourId,$dbconn){
 		$htmlTage = '';
 		$query = "SELECT * from tour_res, location where tour_res.tourid = $1 and tour_res.locationid = location.locationid;";
-		$queryLocationMedia = "SELECT * from location_res, media where location_res.locationid = $1 and media.media_type = 'image'";
+		$queryLocationMedia = "SELECT * from location_res, media where location_res.locationid = $1 and location_res.mediaid = media.mediaid and media.media_type = 'image'";
 
 			$result = pg_prepare($dbconn,"TourData_query", $query);
 			$result2 = pg_prepare($dbconn,"locationImagesQuery",$queryLocationMedi);
@@ -38,9 +38,8 @@ class TourClass
                     
                     $result2 = pg_execute($dbconn,"TourData_query",array($id));
 
-                    $rows =pg_fetch_array($result2);
-                    $imageSrc = $rows['link'];
-
+                    $rowtwo =pg_fetch_array($result2);
+                    $imageSrc = $rowtwo['link'];
 					$htmlTage = $htmlTage."<div class='span2'>
 			          <button onclick=\"show('Page3');\" value='$id' style='text-decoration: none' class='pageButtons btn-default btn-lg btn-block'>
 			      		<span ><img class='imageButtons' img-block src='$imageSrc' width='600px' align='left' ><p class='imgTextTop' align='left'>$name</p><p align='left' class='imgBottomText' >North Wing</p>
