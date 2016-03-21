06b537cde7b3669b45c0c00af06445a08def5233
Hassan Minhas <haminhas@live.com>
2016-03-21 18:03:01 +0000

    json

--- IosApp/index.php
+++ IosApp/index.php
@@ -52,6 +52,7 @@
 			}
 
 			function showPosition(position) {
+			console.log(coTour.length);
 			for(var i = 0; i < coTour.length; i++) {
 			    var obj = coTour[i];
 			    console.log(coTour[i]);
