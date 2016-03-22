ebef0021781415968fca23c9e66d02e5ff0d7fc4
abdi <[nasir2891@googlemail.com]>
2016-03-22 17:35:20 +0000

    Test

--- IosApp/index.php
+++ IosApp/index.php
@@ -283,7 +283,7 @@
 		  <center>
 
 			   <button id="btnHomePage" onclick="show('Page1');"   class="btn btn-success"><p class="HomePageText">HomePage</p></button>
-			    <button id="btnHomePage" onclick="show('Page1');"   class="btn btn-danger"><p class="HomePageText">HomePage</p></button>
+			  
 				  
 			   <label class="topHospitalNameText" id='test123'>Tour Activity</label>
 	
@@ -383,12 +383,11 @@ At the moment it's only pointing at the gallery page , but we can have multiple 
     </div>
 <!--=======================================SECOND PAGE END ============================================--><!--======================================THIRD PAGE START ============================================-->
  <div id="Page3" class="page" style="display:none">
-	 <div class="topHeader">
-   
-<center class="topHeader"><label class="topHospitalNameText" >Royal Brompton Hospital</label> <button id="btnGallery" onclick="show('Page2');"  class="btn btn-primary"><p class="HomePageText">Rooms</p></button></center>
-		
 
-      </div>  
+	 	  <center>
+<center class="topHeader"><label class="topHospitalNameText" >Royal Brompton Hospital</label> <button id="btnGallery" onclick="show('Page2');"  class="btn btn-primary"><p class="HomePageText">Rooms</p></button></center>
+	
+		  </center>
 
 
         
