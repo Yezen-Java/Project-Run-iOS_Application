3cecb2f0cc94df37999abcddaaef66d4f4e266b8
abdi <[nasir2891@googlemail.com]>
2016-03-23 20:36:15 +0000

    sorting video

--- IosApp/index.php
+++ IosApp/index.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap -->
    	<script src="jquery.js"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 	 
