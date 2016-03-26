9a2f0a09214ed37f13ba30b0b40dec9c55f1b96e
Hassan Minhas <haminhas@live.com>
2016-03-26 18:52:35 +0000

    allign

--- IosApp/index.php
+++ IosApp/index.php
@@ -455,7 +455,6 @@ At the moment it's only pointing at the gallery page , but we can have multiple 
     </div>
 <!--==================================SECOND PAGE END ============================================--><!--======================================THIRD PAGE START =======================================-->
  <div id="Page3" class="page" style="display:none">
-s
 	 	  <center>
 <center class="topHeader"><button id="btnGallery" onclick="show('Page2');"  class="btn btn-primary" style="float: left;">Rooms</button>
 <label class="topHospitalNameText" >Gallery</label> 	  
